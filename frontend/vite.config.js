5e5d28c0824f5e02dfd85f67ede19a3c5b53b764
Deepak <dipe345@gmail.com>
2024-11-01 09:32:34 +0100

    deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,11 @@
-// import { defineConfig } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+});
+
 
 // https://vitejs.dev/config/
 // export default defineConfig({
@@ -16,12 +22,11 @@ import react from "@vitejs/plugin-react";
 // 		},
 // 	},
 // });
-import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
+
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-});
+// export default defineConfig({
+//     plugins: [react()],
+// });
 
 
